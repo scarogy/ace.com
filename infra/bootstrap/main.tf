115dd8fcadf8ea8fe4e1926e22087af0cbc6e370
jbekkk <93888734+jbekkk@users.noreply.github.com>
2025-10-03 06:35:13 -0400

    Initial Commit

---
+++ infra/bootstrap/main.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "state" { bucket = var.bucket_name }
+resource "aws_s3_bucket_versioning" "v" {
+  bucket = aws_s3_bucket.state.id
+  versioning_configuration { status = "Enabled" }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "locks" {
+  name         = "tfstate-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "bucket" { value = aws_s3_bucket.state.bucket }
+output "lock_table" { value = aws_dynamodb_table.locks.name }
